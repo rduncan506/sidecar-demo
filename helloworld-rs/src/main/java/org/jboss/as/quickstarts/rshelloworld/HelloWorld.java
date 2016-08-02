a14180060cc0b6dad3a5513d4e199a6cb939dd29
Ronnie Duncan <ronnie.duncan@gmail.com>
2016-08-02 10:52:38 -0300

    Modified to take a name as an argument and POST rather than GET.

--- helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
+++ helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
@@ -17,8 +17,9 @@
 package org.jboss.as.quickstarts.rshelloworld;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -34,18 +35,19 @@ public class HelloWorld {
     @Inject
     HelloService helloService;
 
-    @GET
-    @Path("/json")
+    @POST
+    @Path("/json/{name}")
     @Produces({ "application/json" })
-    public String getHelloWorldJSON() {
-        return "{\"result\":\"" + helloService.createHelloMessage("World") + "\"}";
+    public String getHelloWorldJSON(@PathParam("name") String name) {
+        System.out.println("name: " + name);
+        return "{\"result\":\"" + helloService.createHelloMessage(name) + "\"}";
     }
 
-    @GET
-    @Path("/xml")
+    @POST
+    @Path("/xml/{name}")
     @Produces({ "application/xml" })
-    public String getHelloWorldXML() {
-        return "<xml><result>" + helloService.createHelloMessage("World") + "</result></xml>";
+    public String getHelloWorldXML(@PathParam("name") String name) {
+        System.out.println("name: " + name);
+        return "<xml><result>" + helloService.createHelloMessage(name) + "</result></xml>";
     }
-
 }
